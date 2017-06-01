6673ffc761599c91e6b470540736092d23e85bc4
Susan Woods <swoods63@msn.com>
2017-05-31 23:06:45 -0600

    corrected the conflicts

--- www/src/components/Home.vue
+++ www/src/components/Home.vue
@@ -38,16 +38,9 @@
             <li class="">
               <router-link class="btn" to="/register"><button type="button" class="btn">Register</button></router-link>
             </li>
-            <li class="">
-<<<<<<< HEAD
-              <router-link class="btn" to="/login">Login</router-link>
-=======
               <router-link class="btn" to="/login"><button type="button" class="btn">Login</button></router-link>
->>>>>>> b4846a51d8a254ed2f9ddb05012ca86fa6b99555
-            </li>
           </ul>
->>>>>>> 02ff296ba954272412d4504fc1a8af6dc934d7d6
->>>>>>> ad3d0c8a4961cfc89fb4a983af2c7682b7980ebd
+
         </div>
       </div>
     </div>
@@ -88,21 +81,13 @@
 
   h5 {
     margin-top: 20px;
-<<<<<<< HEAD
     color: blue;
-=======
-    color: white;
->>>>>>> ad3d0c8a4961cfc89fb4a983af2c7682b7980ebd
     text-align: left;
   }
 
   p {
     text-align: left;
-<<<<<<< HEAD
     color: black;
-=======
-    color: white;
->>>>>>> ad3d0c8a4961cfc89fb4a983af2c7682b7980ebd
   }
 
   button {
