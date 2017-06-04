00933d7c3b38db02741f124444f73bc019e46a44
Dustin Richins <dkrichins@gmail.com>
2017-06-03 19:52:49 -0600

    saturday two commit

--- www/src/components/Board.vue
+++ www/src/components/Board.vue
@@ -1,6 +1,19 @@
 <template>
   <div class="board">
     <div class="container">
+      <div class="row">
+        <div class="col-xs-4">
+          <form @submit.prevent="createList">
+          <div class="form-group">
+            <input type="text" class="form-control" v-model="name" placeholder="List Name" required>
+          </div>
+          <div class="form-group">
+            <textarea rows="4" cols="50" class="form-control" v-model="description" placeholder="Description"></textarea>
+          </div>
+            <button class="btn btn-primary" type="submit">Create List</button>
+        </form>
+        </div>
+      </div>
       <div class="row" v-for="list in lists">
         <div class="col-xs-4">
           <div class="well">
@@ -18,6 +31,13 @@
   import List from "./List"
   export default {
     name: 'board',
+    data() {
+    return {
+      name: '',
+      description: '',
+      boardId: this.$route.params.id
+    }
+  },
     mounted() {
       this.$store.dispatch('getBoard', (this.$route.params.id)
       )
@@ -29,6 +49,14 @@
     },
     components: {
       List
+    },
+    methods: {
+      createList(){
+        this.$store.dispatch('createList', {name: this.name, description: this.description, boardId: this.boardId})
+      },
+      removeBoard(board){
+        this.$store.dispatch('removeBoard', board)
+      }
     }
   }
 
